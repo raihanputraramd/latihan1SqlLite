e49c65fc931e7d0589b09081e83f99708682a786
putra <raihanputraramd@gmail.com>
2019-09-09 11:42:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan1sqllite/MainFragment.java
@@ -0,0 +1,100 @@
+package com.example.latihan1sqllite;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainFragment extends Fragment implements View.OnClickListener, RecyclerviewAdapter.OnUserClickListener {
+    RecyclerView recyclerView;
+    EditText edtName, edtAge;
+    Button btnSubmit;
+    RecyclerView.LayoutManager layoutManager;
+    Context context;
+    List<PersonBean> listPersonInfo;
+
+    public MainFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getActivity();
+        recyclerView = view.findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+
+        edtName = view.findViewById(R.id.edtName);
+        edtAge = view.findViewById(R.id.edtAge);
+        btnSubmit = view.findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(this);
+
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+        listPersonInfo = databaseHelper.selectUserData();
+
+        RecyclerviewAdapter adapter = new RecyclerviewAdapter(context, listPersonInfo, this);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnSubmit) {
+            DatabaseHelper databaseHelper = new DatabaseHelper(context);
+            PersonBean currentPerson = new PersonBean();
+            String btnStatus = btnSubmit.getText().toString();
+            if (btnStatus.equals("Submit")) {
+                currentPerson.setName(edtName.getText().toString());
+                currentPerson.setAge(Integer.parseInt(edtAge.getText().toString()));
+                databaseHelper.insert(currentPerson);
+            }if (btnStatus.equals("Update")) {
+                currentPerson.setName(edtName.getText().toString());
+                currentPerson.setAge(Integer.parseInt(edtAge.getText().toString()));
+                databaseHelper.update(currentPerson);
+            }
+            setupRecyclerView();
+            edtName.setText("");
+            edtAge.setText("");
+            edtName.setFocusable(true);
+            btnSubmit.setText("Submit");
+
+        }
+    }
+
+    @Override
+    public void onUserClick(PersonBean currentPerson, String action) {
+        if (action.equals("Edit")) {
+            edtName.setText(currentPerson.getName());
+            edtName.setFocusable(false);
+            edtAge.setText(currentPerson.getAge() + "");
+            btnSubmit.setText("Edit");
+        }
+        if (action.equals("Delete")) {
+            DatabaseHelper databaseHelper = new DatabaseHelper(context);
+            databaseHelper.delete(currentPerson.getName());
+            setupRecyclerView();
+        }
+    }
+}
